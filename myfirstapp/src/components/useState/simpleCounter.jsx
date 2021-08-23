879bdb9b09767bc84a5317ea6195d9812151b02c
Piyush Jain <piyushmbm45@gmail.com>
2021-08-23 20:35:46 +0530

    sync and async approach in counter

--- myfirstapp/src/components/useState/simpleCounter.jsx
+++ myfirstapp/src/components/useState/simpleCounter.jsx
@@ -4,7 +4,12 @@ function Simplecounter(){
 
 const [value, setValue] = useState(0);
 
-
+function handleClick(){
+    setTimeout(() => {
+        setValue(value+1)
+        // if press button more than one time(in 2 sec) then still it will increase by one
+    }, 2000);
+}
 
     return(
         <div>
@@ -14,6 +19,9 @@ const [value, setValue] = useState(0);
         <button onClick={()=>{setValue(0)}}>Reset</button>
         <button onClick={()=>{setValue(value-1)}}>Decrease</button>
         <hr></hr>
+        <h1>Complex Counter</h1>
+        <h2>{value}</h2>
+        <button onClick={handleClick}>Increase Later</button>
         </div>
     )
 }
