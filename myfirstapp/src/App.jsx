bc41d974c3207c63b58e0fd64db67dad33d9b8f9
Piyush Jain <piyushmbm45@gmail.com>
2021-08-25 18:02:48 +0530

    setup useEffect Cleanup

--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 // import Setup from "./components/useState/UseStateObject"
 // import Setup from './components/useState/simpleCounter'
-import Setup from "./components/useEffect/1-useEffect-Basic"
+// import Setup from "./components/useEffect/1-useEffect-Basic"
+import Setup from "./components/useEffect/2-useEffect-cleanup"
 
 const App = () => {
   return ( <Setup/>
---
+++ myfirstapp/src/components/useEffect/2-useEffect-cleanup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function UseEffectCleanUp(){
+
+    return <h1>useEffect CleanUp</h1>
+}
+
+
+
+export default UseEffectCleanUp;
