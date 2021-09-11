540cfdefb3a18883ea94d87bf97a90dac4bd63b7
piyushmbm45 <piyushmbm45@gmail.com>
2021-09-11 22:19:18 +0530

    ternary opeartor

--- myfirstapp/src/components/conditionalRendering/2-short-circuit.jsx
+++ myfirstapp/src/components/conditionalRendering/2-short-circuit.jsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 
 function ShortCircuit() {
   const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  const secondValue = text && "And Hello World";
+  const [isError, setError] = useState(false)
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "And Hello World";
   return (
     <>
       {/* <h1>{firstValue}</h1>
       <h1>value : {secondValue}</h1> */}
-      <h1>{text || "Hello World From OR"}</h1>
-      {text && <h1>Hello World From &&</h1>}
+      <h1>{text || "Piyush Jain"}</h1>
+      <button onClick={()=>{setError(!isError)}}>Toggle Error</button>
+      {isError && <h1>Error...</h1>}
+      {/* using ternary operator */}
+      {isError ? <p>there is an error</p>: <p>no error</p>}
     </>
   );
 }
