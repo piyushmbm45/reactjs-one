77ca6d5625a2c21866a4a6d455b3af276003ec94
Piyush Jain <piyushmbm45@gmail.com>
2021-09-14 19:59:50 +0530

    multiple return

--- myfirstapp/src/components/conditionalRendering/1-multipleReturns.jsx
+++ myfirstapp/src/components/conditionalRendering/1-multipleReturns.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
+const url2 ="https://jsonplaceholder.typicode.com/users";
 
 function MultipleReturns() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState("default User");
 
-  useEffect(() => {
-    fetch(url)
+  // useEffect(() => {
+    fetch(url2)
       .then((res) => {
         if (res.status >= 200 && res.status <= 299) {
           return res.json();
@@ -23,7 +24,7 @@ function MultipleReturns() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  // }, []);
 
   //  multiple return to website these are just one line of html we can send much more data
   if (isLoading) {
@@ -34,10 +35,10 @@ function MultipleReturns() {
   }
   return (
     <div>
-      <h1>{user.name}</h1>
-      <h2>{user.login}</h2>
+      <h1>{user[0].name}</h1>
+      {/* <h2>{user.login}</h2>
       <p>{user.blog}</p>
-      <img src={user.avatar_url} alt=""/>
+      <img src={user.avatar_url} alt=""/> */}
     </div>
   );
 }
