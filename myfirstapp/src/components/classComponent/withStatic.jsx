555b2b59f5c993e3be5de062d7596236b8fbb7fb
piyushmbm45 <piyushmbm45@gmail.com>
2021-11-24 21:28:35 +0530

    more complex satic version

--- myfirstapp/src/components/classComponent/withStatic.jsx
+++ myfirstapp/src/components/classComponent/withStatic.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+const bio = { fName: "piyush", lName: "jain" };
+
 class WithReact extends React.Component {
   render() {
     return (
       <div>
         <p>My Fav Color Red</p>
-        <MyClassProp data={"yellow"} />
+        <MyClassProp data={"yellow"} bio={bio} />
       </div>
     );
   }
@@ -14,15 +16,15 @@ class WithReact extends React.Component {
 class MyClassProp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { color: "red" };
+    this.state = { color: "red" , bioN: {}};
   }
   static getDerivedStateFromProps(props, state) {
-      console.log(props);
-      console.log(state);
-    return { color: props.data };
+    console.log(props);
+    console.log(state);
+    return { color: props.data , bioN: props.bio};
   }
   render() {
-    return <div style={{ color: `${this.state.color}` }}>Hello World</div>;
+    return <div style={{ color: `${this.state.color}` }}>Hello World {this.state.bioN.fName} {this.state.bioN.lName}</div>;
   }
 }
 
