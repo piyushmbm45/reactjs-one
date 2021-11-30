7e5d4e12a7d8676a62f0901b0af04104fd35323d
piyushmbm45 <piyushmbm45@gmail.com>
2021-11-30 20:41:48 +0530

    getsnap classcomponent

--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -15,7 +15,8 @@ import React from 'react'
 // import Setup from './components/classComponent/withStatic'
 // import Setup from './components/classComponent/shouldComponent'
 // import Setup from './components/classComponent/didUpdate'
-import Setup from './components/classComponent/state'
+// import Setup from './components/classComponent/state'
+import Setup from './components/classComponent/getSnap'
 
 
 const App = () => {
